23ae652a450bda79e7728d7e182deabbbe246880
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-12-29 00:12:33 +0700

    'map-succes'

--- components/BoxTop.tsx
+++ components/BoxTop.tsx
@@ -1,40 +1,44 @@
-import React from 'react'
-import { CardMedia, Container } from '@mui/material';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import { width } from '@mui/system';
-import axios from 'axios';
-import {useQuery} from 'react-query';
+import React from "react";
+import { CardMedia, Container } from "@mui/material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import { width } from "@mui/system";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 function BoxTop() {
-    let fetchdog = async () => {
-        const {data} = await axios.get('https://dog.ceo/api/breeds/image/random/6');
-        return data;
-    }
-    fetchdog();
-    const{data,error,isError,isLoading} = useQuery('dog',fetchdog);
-    if (isError) {
-        return <div>Error</div>
-    }
-    if (isLoading) {
-        return <div>Loading</div>
-    }
-
+  let fetchdog = async () => {
+    const { data } = await axios.get(
+      "https://dog.ceo/api/breeds/image/random/6"
+    );
+    return data;
+  };
+  fetchdog();
+  const { data, error, isError, isLoading } = useQuery("dog", fetchdog);
+  if (isError) {
+    return <div>Error</div>;
+  }
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  let array: string[] = data.message;
+  let hehe = array.map((item: string) => {
     return (
-        <div>
-            <Container>
-                <Card sx={{ bgcolor: '#cfe8fc', height: '500px',width: '900px' }}>
-                    <CardMedia>
-                        <img src={data.message[0]} alt="dog" />
+      
+        <Container >
+            
+          <Card sx={{ bgcolor: "#cfe8fc", height: "100px", width: "90px" }}>
+            <CardMedia>
+              <img src={item} height='100' width = '90' alt="dog" />
+            </CardMedia>
+          </Card>
+            
+        </Container>
+      
+    );
+  });
+    return <div className = 'Box-Top'>{hehe}</div>;
 
-                    </CardMedia>
-                    </Card>
-                    
-                        
-                    
-                </Container>
-        </div>
-    )
 }
 
-export default BoxTop
+export default BoxTop;
