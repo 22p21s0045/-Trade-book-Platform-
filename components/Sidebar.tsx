cfbcb1a089c2c053087c677c93aa8db6845c7e54
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-01-05 15:25:10 +0700

    'add-sidebar'

---
+++ components/Sidebar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { FaBeer } from 'react-icons/fa';
+function Sidebar() {
+    return (
+        <div>
+            <ProSidebar>
+  <Menu iconShape="square">
+    <MenuItem icon={<FaBeer />}>Dashboard</MenuItem>
+    <SubMenu title="Components" icon={<FaBeer />}>
+      <MenuItem>Component 1</MenuItem>
+      <MenuItem>Component 2</MenuItem>
+    </SubMenu>
+  </Menu>
+</ProSidebar>
+        </div>
+    )
+}
+
+export default Sidebar
