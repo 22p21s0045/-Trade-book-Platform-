5b2eae1df8ee1deddbe0f327b73fa812bc251160
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-01-06 19:14:11 +0700

    'add-icon-Sidebar'

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer,FaHotjar } from 'react-icons/fa';
+import { VscFeedback } from "react-icons/vsc";
+import { MdHistory } from "react-icons/md";
+import { GrAnnounce } from "react-icons/gr";
+import { IconContext } from "react-icons";
+import { color } from '@mui/system';
 function Sidebar() {
     return (
         <div>
             <ProSidebar width = "180px">
   <Menu iconShape="square">
-    <MenuItem icon={<FaBeer />}>Dashboard</MenuItem>
+  <SubMenu title="หมวดหมู่">
+      <MenuItem>จิตวิทยา</MenuItem>
+      <MenuItem>ภาษา</MenuItem>
+    </SubMenu>
+    <MenuItem icon={<FaBeer />}>โพสบันทึก</MenuItem>
+    <MenuItem icon = {<VscFeedback />}>ฟีดเเบค</MenuItem>
+    <MenuItem  icon={<FaBeer />}>ประมูล</MenuItem>
+    
+    <MenuItem icon = {<FaHotjar />}>มาเเรง</MenuItem>
+    <MenuItem icon = {<MdHistory/>}>ประวัติเเลก</MenuItem>
+    <MenuItem icon = {<GrAnnounce />}>ประกาศ</MenuItem>
+
+
     <SubMenu title="Components" icon={<FaBeer />}>
-      <MenuItem>Component 1</MenuItem>
+      <MenuItem icon = {<VscFeedback />}>ฟีดเเบค</MenuItem>
       <MenuItem>Component 2</MenuItem>
     </SubMenu>
   </Menu>
